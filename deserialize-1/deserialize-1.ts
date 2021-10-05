7f77dc0b37d50953e7447a2f8e69b3fc7cc3d2c7
Sam Thursfield <sam.thursfield@codethink.co.uk>
2021-10-05 17:49:41 +0200

    Fill in some more deserializer code
    This currently fails:
    
        npx tsc ./deserialize-1.ts ; and node ./deserialize-1.js
        deserialize-1.ts:75:9 - error TS2322: Type '(Path | AccessControlled<Definition<ta, va>>)[][]' is not assignable to type '[Path, AccessControlled<Definition<ta, va>>][]'.
          Type '(Path | AccessControlled<Definition<ta, va>>)[]' is not assignable to type '[Path, AccessControlled<Definition<ta, va>>]'.
            Target requires 2 element(s) but source may have fewer.
    
        75         Modules: input['modules'].map(decodeArrayElement)
                   ~~~~~~~
    
          ../sample-output-morphir-ir/morphir/ir/Package.ts:15:5
            15     Modules: Array<[Morphir_IR_Module.ModuleName, Morphir_IR_AccessControlled.AccessControlled<Morphir_IR_Module.Definition<ta, va>>]>;
                   ~~~~~~~
            The expected type comes from property 'Modules' which is declared here on type 'Definition<ta, va>'
    
        Found 1 error.

--- deserialize-1/deserialize-1.ts
+++ deserialize-1/deserialize-1.ts
@@ -7,25 +7,81 @@ const INPUT = './morphir-ir.json';
 type serializedMorphir_IR_Name_Name = Array<string>;
 type serializedMorphir_IR_Path_Path = Array<serializedMorphir_IR_Name_Name>;
 type serializedMorphir_IR_Package_Name = serializedMorphir_IR_Path_Path;
+type serializedMorphir_IR_Module_ModuleName = serializedMorphir_IR_Path_Path;
+type serializedMorphir_IR_AccessControlled_AccessControlled<a> = [
+    // This type has a custom Elm codec/decodec, it's not serialized like other Record types.
+    string, a
+]
+type serializedMorphir_IR_Module_Definition<ta,va> = any; // FIXME: Placeholder
+type serializedMorphir_IR_Package_Definition<ta,va> = {
+    // This type has a custom Elm codec/decodec, it's not serialized like other Record types.
+    'modules': [
+        {
+            'name': serializedMorphir_IR_Module_ModuleName,
+            'def': serializedMorphir_IR_AccessControlled_AccessControlled<serializedMorphir_IR_Module_Definition<ta,va>>,
+        }
+    ]
+}
 type serializedMorphir_IR_Distribution_Distribution = [
     // FIXME: the JSON serialization uses "library" while the
     // generated TypeScript types use "Library".
     "library",
     serializedMorphir_IR_Package_Name,
-    any, //Array<[Morphir.IR.Package.PackageName, Morphir.IR.Package.Specification<[]>]>,
-    any, //Morphir.IR.Package.Definition<[], Morphir.IR.Type.Type<[]>>
+    any, // FIXME: Placeholder
+    serializedMorphir_IR_Package_Definition<[],[]>,
 ];
 
 function decodePath(input: serializedMorphir_IR_Path_Path): Morphir.IR.Path.Path {
     return input;
 }
 
+function decodeAccess(input: string): Morphir.IR.AccessControlled.Access {
+    if (input == 'public') {
+        return { kind: "Public" };
+    } else if (input == 'private') {
+        return { kind: "Private" };
+    } else {
+        throw new Error("Invalid access type: " + input);
+    }
+}
+
+function decodeAccessControlled<VST,VT>(input: serializedMorphir_IR_AccessControlled_AccessControlled<VST>,
+                                        decodeValue: (VST) => VT): Morphir.IR.AccessControlled.AccessControlled<VT> {
+    // This type has a custom Elm codec/decodec.
+    return {
+        Access: decodeAccess(input[0]),
+        Value: decodeValue(input[1]),
+    }
+}
+
+function decodeModuleName(input: serializedMorphir_IR_Module_ModuleName): Morphir.IR.Module.ModuleName {
+    return decodePath(input);
+}
+
+function decodePackageDefinition<ta,va>(input: serializedMorphir_IR_Package_Definition<ta,va>): Morphir.IR.Package.Definition<ta,va> {
+    // This type has a custom Elm codec/decodec.
+    function decodeArrayElement(item) {
+        return [
+            decodeModuleName(input['name']),
+            decodeAccessControlled<serializedMorphir_IR_Module_Definition<ta,va>,Morphir.IR.Module.Definition<ta,va>>(
+                input['def'],
+                (item: serializedMorphir_IR_Module_Definition<ta,va>): Morphir.IR.Module.Definition<ta,va> => {
+                    return item.name, item.def
+                }
+            ),
+        ]
+    };
+    return {
+        Modules: input['modules'].map(decodeArrayElement)
+    };
+};
+
 function decodeDistribution(input: serializedMorphir_IR_Distribution_Distribution): Morphir.IR.Distribution.Distribution {
     return {
         kind: "Library",
         arg1: decodePath(input[1]),
         arg2: input[2],
-        arg3: decodePackageDefinition(input[3]),
+        arg3: decodePackageDefinition<[],Morphir.IR.Type.Type<[]>>(input[3]),
     }
 }
 
